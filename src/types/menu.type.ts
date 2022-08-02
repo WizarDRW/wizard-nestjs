cf3befbe029c2dcbeeb5e0c02331c88a81766fb2
WizarDRW <wnokroot@gmail.com>
2022-08-02 18:27:07 +0300

    Menu type is created.

---
+++ src/types/menu.type.ts
@@ -0,0 +1,19 @@
+import { ObjectType, Field } from "@nestjs/graphql";
+import { IMenuModel } from "src/interfaces/models/menu.model.interface";
+import { MongoType } from "./mongo.type";
+
+@ObjectType()
+export class MenuType extends MongoType implements IMenuModel {
+    @Field({ nullable: true })
+    name: String;
+    @Field({ nullable: true })
+    description: String;
+    @Field({ nullable: true })
+    path: String;
+    @Field({ nullable: true })
+    status: Boolean;
+    @Field({ nullable: true })
+    sort: String;
+    @Field({ nullable: true })
+    children: Object;
+}
