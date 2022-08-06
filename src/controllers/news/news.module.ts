580f852c4b951a7f22e315e05f56c7a0e484a621
WizarDRW <wnokroot@gmail.com>
2022-08-06 21:08:05 +0300

    News controller's module created.

--- src/controllers/news/news.module.ts
+++ src/controllers/news/news.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MalcolmXService } from 'src/middlewares/proxies/malcolmx.proxy';
+import { NewsSchema } from 'src/schemas/news.schema';
 import { NewsResolver } from './news.resolver';
+import { NewsService } from './news.service';
 
 @Module({
-  providers: [NewsResolver]
+  imports: [MongooseModule.forFeature([{ name: "News", schema: NewsSchema }])],
+  providers: [NewsResolver, NewsService, MalcolmXService]
 })
 export class NewsModule {}
