21a765951088c21a81af3b02db64557bb623f822
WizarDRW <wnokroot@gmail.com>
2022-08-19 10:02:39 +0300

    Chapter resolver updated.

--- src/controllers/chapter/chapter.resolver.ts
+++ src/controllers/chapter/chapter.resolver.ts
@@ -1,4 +1,29 @@
 import { Resolver } from '@nestjs/graphql';
+import { IChapterModel } from 'src/interfaces/models/chapter.model.interface';
+import { IChapterResolver } from 'src/interfaces/resolvers/chapter.resolver.interface';
+import { MalcolmXService } from 'src/middlewares/proxies/malcolmx.proxy';
+import { IMongoType } from 'src/middlewares/utils/mongo.util';
+import { ChapterService } from './chapter.service';
 
 @Resolver()
-export class ChapterResolver {}
+export class ChapterResolver implements IChapterResolver {
+    constructor(private service: ChapterService, private proxy: MalcolmXService<IChapterModel>) {
+        this.proxy = new MalcolmXService<IChapterModel>(this.service);
+    }
+
+    chapters(): Promise<IChapterModel[]> {
+        return this.proxy.findAll()
+    }
+    chapterById(id: String): Promise<IChapterModel> {
+        return this.proxy.findById(id);
+    }
+    chapterCreate(input: IChapterModel): Promise<IChapterModel> {
+        return this.proxy.create(input);
+    }
+    chapterUpdate(input: IChapterModel): Promise<IMongoType> {
+        return this.proxy.update(input);
+    }
+    chapterDelete(id: String): Promise<IMongoType> {
+        return this.proxy.delete(id);
+    }
+}
