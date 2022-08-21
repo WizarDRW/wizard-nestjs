aa7bc50275dd2639f750ace32812038ad473ad00
WizarDRW <wnokroot@gmail.com>
2022-08-21 12:16:39 +0300

    User option module updated.

--- src/controllers/useroption/user.option.module.ts
+++ src/controllers/useroption/user.option.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserOptionSchema } from 'src/schemas/useroption.schema';
 import { UserOptionService } from './user.option.service';
-import { UseroptionResolver } from './user.option.resolver';
+import { UserOptionResolver } from './user.option.resolver';
 import { MalcolmXService } from 'src/middlewares/proxies/malcolmx.proxy';
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: "UserOptions", schema: UserOptionSchema }])],
-    providers: [UserOptionService, UseroptionResolver, MalcolmXService],
+    providers: [UserOptionService, UserOptionResolver, MalcolmXService],
 })
 export class UserOptionModule {}
