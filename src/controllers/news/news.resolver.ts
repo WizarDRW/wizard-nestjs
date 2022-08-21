16d0289cd92e64302a2288014571223895d5759c
WizarDRW <wnokroot@gmail.com>
2022-08-21 12:15:07 +0300

    News resolver updated.

--- src/controllers/news/news.resolver.ts
+++ src/controllers/news/news.resolver.ts
@@ -1,4 +1,29 @@
 import { Resolver } from '@nestjs/graphql';
+import { INewsModel } from 'src/interfaces/models/news.model.interface';
+import { INewsResolver } from 'src/interfaces/resolvers/news.resolver.interface';
+import { MalcolmXService } from 'src/middlewares/proxies/malcolmx.proxy';
+import { IMongoType } from 'src/middlewares/utils/mongo.util';
+import { NewsService } from './news.service';
 
 @Resolver()
-export class NewsResolver {}
+export class NewsResolver implements INewsResolver {
+    constructor(private service: NewsService, private proxy: MalcolmXService<INewsModel>) {
+        this.proxy = new MalcolmXService<INewsModel>(this.service);
+    }
+
+    news(): Promise<INewsModel[]> {
+        throw new Error('Method not implemented.');
+    }
+    newsById(id: String): Promise<INewsModel> {
+        throw new Error('Method not implemented.');
+    }
+    newsCreate(input: INewsModel): Promise<INewsModel> {
+        throw new Error('Method not implemented.');
+    }
+    newsUpdate(input: INewsModel): Promise<IMongoType> {
+        throw new Error('Method not implemented.');
+    }
+    newsDelete(id: String): Promise<IMongoType> {
+        throw new Error('Method not implemented.');
+    }
+}
